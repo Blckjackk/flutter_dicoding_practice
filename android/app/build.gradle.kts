5dc34f16c3cba9ca7b7e0d4d3af39f614ed1a526
Blckjackk <diegomortar@gmail.com>
2025-04-04 09:54:41 +0700

    Initial commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+android {
+    compileSdkVersion 33
+    defaultConfig {
+        minSdkVersion 21
+        targetSdkVersion 33
+    }
+    namespace = "com.example.belajar_flutter_dicoding"
+    compileSdk = flutter.compileSdkVersion
+    // Ganti baris ini
+    // ndkVersion = flutter.ndkVersion
+    ndkVersion = null // Menonaktifkan NDK
+    // Atau gunakan versi spesifik:
+    // ndkVersion = "21.4.7075529"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.belajar_flutter_dicoding"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
